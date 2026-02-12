1980466fde053675634bc6de625310dabb42b257
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2026-02-12 13:18:10 +0330

    Remove redundant log

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,6 @@ import * as vscode from "vscode";
 import { convertSelectionOrDocument } from "./commands/convertSelectionOrDocument";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log("CSS Color Converter activated");
-
     const run = () => convertSelectionOrDocument();
 
     context.subscriptions.push(
